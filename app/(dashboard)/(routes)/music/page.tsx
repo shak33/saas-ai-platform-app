847a5a7401cb2b81776846468b4895c6f7951e50
shak33 <maciej@poppek.pl>
2023-07-28 04:29:18 +0200

    Installed Replicate package, added new field to enviroemtn file, added functionality to generate music with Replicate AI

---
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { useState } from 'react';
+import { Music } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+import Heading from '@/components/Heading';
+import { Form, FormField, FormItem, FormControl } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from "@/components/ui/button";
+import { Empty } from '@/components/Empty';
+import { Loader } from '@/components/Loader';
+
+import { formSchema } from './constants';
+
+const MusicPage = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: '',
+    },
+  });
+  const [music, setMusic] = useState<string>('');
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic('');
+
+      const response = await axios.post('/api/music', values);
+
+      setMusic(response.data.audio);
+      form.reset();
+    } catch (error) {
+      // TODO: Open Pro Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Music Generation"
+        description="Turn your prompt into music."
+        icon={Music}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-whithin:shadow-sm grid grid-cols-12 gap-2"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="What music do you want us to create?"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && (
+            <Empty
+              label="No music generated."
+            />
+          )}
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MusicPage;
